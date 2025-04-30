5c8fcce8c06e5fa0990b59226d661df7b0e83477
higorsantana-omega <higor.santanahs17@gmail.com>
2025-04-30 19:44:15 -0300

    feat(auth): implement authentication functionality for user login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,12 +1,17 @@
-import { ConflictException, Injectable } from '@nestjs/common'
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 
-import { hash } from 'bcryptjs'
+import { compare, hash } from 'bcryptjs'
 
 import { UsersRepository } from 'src/shared/module/database/repositories/users.repository'
 import { User, type UserData } from 'src/shared/entity/user.entity'
 
 import { SignupDto } from './dto/signup.dto'
+import { AuthenticateDto } from './dto/authenticate.dto'
 
 @Injectable()
 export class AuthService {
@@ -14,6 +19,23 @@ export class AuthService {
     private readonly usersRepository: UsersRepository,
     private readonly jwtService: JwtService
   ) {}
+
+  async authenticate(authenticateDto: AuthenticateDto) {
+    const user = await this.usersRepository.findOneBy({
+      email: authenticateDto.email
+    })
+    if (!user) throw new UnauthorizedException('Invalid credentials.')
+
+    const isSamePassword = await compare(
+      authenticateDto.password,
+      user.getPassword()
+    )
+    if (!isSamePassword) throw new UnauthorizedException('Invalid credentials.')
+
+    const accessToken = await this.generateAccessToken(user.getId())
+
+    return { accessToken }
+  }
 
   async signup(signupDto: SignupDto) {
     const emailTaken = await this.usersRepository.findOneBy({
