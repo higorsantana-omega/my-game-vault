47b918ba2d3a0c1ab22ac3eedd4d1ceb14824a8c
higorsantana-omega <higor.santanahs17@gmail.com>
2025-04-30 19:27:07 -0300

    feat(auth): implement zod schema for signup DTO with validation rules for name, email and password

---
+++ src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,27 @@
+import { createZodDto } from 'nestjs-zod'
+import { z } from 'zod'
+
+const signupSchema = z.object({
+  name: z
+    .string({ required_error: 'Name is required' })
+    .min(2, { message: 'Name must be at least 2 characters long' })
+    .max(50, { message: 'Name must be at most 50 characters long' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Invalid email format' }),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .regex(/[A-Z]/, {
+      message: 'Password must contain at least one uppercase letter'
+    })
+    .regex(/[a-z]/, {
+      message: 'Password must contain at least one lowercase letter'
+    })
+    .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+    .regex(/[^A-Za-z0-9]/, {
+      message: 'Password must contain at least one special character'
+    })
+})
+
+export class SignupDto extends createZodDto(signupSchema) {}
