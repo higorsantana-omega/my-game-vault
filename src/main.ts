106f9b2b20d65fc7bde371286945d8d16d9e7726
higorsantana-omega <higor.santanahs17@gmail.com>
2025-04-30 19:20:40 -0300

    feat(main): implement API prefix and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import 'dotenv/config'
 
 import { NestFactory } from '@nestjs/core'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+  app.enableVersioning()
+
   await app.listen(process.env.PORT ?? 3000)
 }
+
 bootstrap()
