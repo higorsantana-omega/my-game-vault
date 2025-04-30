37b2a950a7d8940a07eed46468f6df15d650dc65
higorsantana-omega <higor.santanahs17@gmail.com>
2025-04-29 21:19:55 -0300

    feat(database): create database module and service using prisma

---
+++ src/shared/module/database/prisma.service.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  Logger,
+  OnApplicationShutdown,
+  OnModuleDestroy,
+  OnModuleInit
+} from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+import { env } from 'src/shared/config/env'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy, OnApplicationShutdown
+{
+  private logger = new Logger(PrismaService.name)
+
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: env.DATABASE_URL
+        }
+      }
+    })
+  }
+
+  async onModuleInit() {
+    this.logger.log('Connecting to Prisma...')
+
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    this.logger.log('Disconnecting from Prisma...')
+
+    await this.$disconnect()
+  }
+
+  onApplicationShutdown(signal: string) {
+    this.logger.log(`Application shutdown signal received: ${signal}`)
+    this.$disconnect().catch((err) =>
+      this.logger.error('Error during Prisma disconnection:', err)
+    )
+  }
+}
