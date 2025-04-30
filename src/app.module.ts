e57134c924b64faf40437ae42d40e45ddcfb5189
higorsantana-omega <higor.santanahs17@gmail.com>
2025-04-30 19:32:03 -0300

    chore(module): add DatabaseModule and AuthModule as imports, remove AppController from controllers, add ZodValidationPipe to enable schema validation in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { APP_PIPE } from '@nestjs/core'
+
+import { ZodValidationPipe } from 'nestjs-zod'
+
+import { DatabaseModule } from './shared/module/database/database.module'
+
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [DatabaseModule, AuthModule],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe
+    }
+  ]
 })
 export class AppModule {}
