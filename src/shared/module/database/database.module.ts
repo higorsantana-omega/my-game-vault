84497ec0265f11a5920ad525f8111dc817649879
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-01 15:28:12 -0300

    feat(database): add GamesRepository to the DatabaseModule

--- src/shared/module/database/database.module.ts
+++ src/shared/module/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common'
 import { PrismaService } from './prisma.service'
 import { UsersRepository } from './repositories/users.repository'
+import { GamesRepository } from './repositories/games.repository'
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository]
+  providers: [PrismaService, UsersRepository, GamesRepository],
+  exports: [UsersRepository, GamesRepository]
 })
 export class DatabaseModule {}
