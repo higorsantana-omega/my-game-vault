f33209ed87c6257f3f975a46de7ef088810dcbdf
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-01 14:47:40 -0300

    chore(env): add RAWG_API_KEY

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -2,7 +2,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().min(1, 'DATABASE_URL is required'),
-  JWT_SECRET: z.string().min(1, 'JWT_SECRET is required')
+  JWT_SECRET: z.string().min(1, 'JWT_SECRET is required'),
+  RAWG_API_KEY: z.string().min(1, 'RAWG_API_KEY is required')
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
