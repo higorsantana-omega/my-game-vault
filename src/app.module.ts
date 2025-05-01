69a922d0380152c8cd53d9efba460fef7399ea0c
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-01 13:04:34 -0300

    feat(app.module): add UsersModule to the app module and AuthGuard as providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common'
-import { APP_PIPE } from '@nestjs/core'
+import { APP_GUARD, APP_PIPE } from '@nestjs/core'
 
 import { ZodValidationPipe } from 'nestjs-zod'
 
 import { DatabaseModule } from './shared/module/database/database.module'
 
+import { AuthGuard } from './modules/auth/auth.guard'
+
 import { AuthModule } from './modules/auth/auth.module'
+import { UsersModule } from './modules/users/users.module'
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, UsersModule],
   controllers: [],
   providers: [
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
     }
   ]
 })
