06b01f07fb1a2080269d83603bb00feccbf88d7d
higorsantana-omega <higor.santanahs17@gmail.com>
2025-05-01 21:30:29 -0300

    chore(env): add redis config and set default values

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -3,7 +3,10 @@ import { z } from 'zod'
 const envSchema = z.object({
   DATABASE_URL: z.string().min(1, 'DATABASE_URL is required'),
   JWT_SECRET: z.string().min(1, 'JWT_SECRET is required'),
-  RAWG_API_KEY: z.string().min(1, 'RAWG_API_KEY is required')
+  RAWG_API_KEY: z.string().min(1, 'RAWG_API_KEY is required'),
+  REDIS_HOST: z.string().optional().default('127.0.0.1'),
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_DB: z.coerce.number().optional().default(0)
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
